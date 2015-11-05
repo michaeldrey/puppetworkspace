47574ddd4f4fa843677223efa23026337954f1c8
Mike Dreyfus <michaeldrey825@gmail.com>
2015-11-05 12:26:28 -0500

    allows dynamic user

--- install.pp
+++ install.pp
@@ -4,11 +4,14 @@ class workstation::install {
 #based on OS
 
 
+if $::id == 'root' {
+  $userpath = "/root"
+}
+else{
+  $userpath = "/home/${::id}"
+}
 
 
-
-
- $user =$::id 
 
  #Ensures vim and git and working directories are present
 
@@ -18,28 +21,28 @@ class workstation::install {
   package{ 'git':
     ensure  => 'present',	
 }
-  file {['/root/.vim', '/root/.vim/bundle/','/root/.vim/autoload']:
-    ensure => 'directory',
+  file {["${userpath}/.vim","${userpath}/.vim/bundle/","${userpath}/.vim/autoload"]:
+    ensure  => 'directory',
 }
 
 #Pulls down pathogen and places it in ~/.vim/autoload
 exec { 'pathogen':
-  command => '/usr/bin/curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim',
+    command => '/usr/bin/curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim',
 }
 
 #creates the .vimrc file
- file { '/root/.vimrc':
-    ensure  => 'file',
-    content => "call pathogen#infect() \nsyntax on \nfiletype plugin indent on",
+ file { "${userpath}/.vimrc":
+    ensure   => 'file',
+    content  => "call pathogen#infect() \nsyntax on \nfiletype plugin indent on",
     require  => Exec['pathogen'],
 }	
 
 #Pullls down vim-puppet.vim and tabular and puts them in /bundle
 
   exec { 'vim-puppet':
-    cwd => "/root/.vim/bundle",
-    command => "/usr/bin/git clone https://github.com/rodjek/vim-puppet.git; /usr/bin/git clone https://github.com/godlygeek/tabular.git;",
-    require  => [File['/root/.vim', '/root/.vim/bundle/','/root/.vim/autoload'], Package['git']],
+    cwd      => "${userpath}/.vim/bundle",
+    command  => "/usr/bin/git clone https://github.com/rodjek/vim-puppet.git; /usr/bin/git clone https://github.com/godlygeek/tabular.git;",
+    require  => [File["${userpath}/.vim","${userpath}/.vim/bundle/","${userpath}/.vim/autoload"], Package['git']],
 }
 
 }
